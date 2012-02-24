e2cbd7b49eef605cd75099c343b67a3bd8d5061c
leomc1988 <leomc1988@gmail.com>
2012-02-24 01:15:07 +0000

    ontab goal ,click check in to wipe out goal's tag on map

--- CampusNaut/src/ut/ece1778/campusnaut/CurrentGameOverlay.java
+++ CampusNaut/src/ut/ece1778/campusnaut/CurrentGameOverlay.java
@@ -3,6 +3,9 @@ package ut.ece1778.campusnaut;
 import java.util.ArrayList;
 import java.util.List;
 
+import ut.ece1778.bean.Game;
+import ut.ece1778.bean.Goal;
+
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.animation.Animation;
@@ -21,22 +24,33 @@ public class CurrentGameOverlay extends ItemizedOverlay<OverlayItem>{
 	private TextView goalTitle;
 	private Animation myAnimation_Alpha;
 	private List<OverlayItem> items = new ArrayList<OverlayItem>();
+	private Game game;
 	
+	
+
 	public CurrentGameOverlay(Context context ,Drawable marker,
-			LinearLayout checkinLayout,TextView goalTitle) {
+			LinearLayout checkinLayout,TextView goalTitle, Game game) {
 		super(marker);
 		this.context = context;
 		this.checkinLayout = checkinLayout;
 		this.goalTitle = goalTitle;
+		this.game = game;
 		// TODO Auto-generated constructor stub
 		boundCenterBottom(marker);
+		ArrayList<Goal> goals = game.getGoals();
 		
-		items.add(new OverlayItem(new GeoPoint((int)(43663846.65313048),(int)(-79399258.94474051))
-			,"ALable1","ALable2"));
-		items.add(new OverlayItem(new GeoPoint((int)(43664800.38890235),(int)(-79398200.84819761))
-			,"BLable1","BLable2"));
-		items.add(new OverlayItem(new GeoPoint(43663347,-79399759)
-			,"CLable1","CLable2"));
+		for(int i = 0;i < goals.size();i++){
+			items.add(new OverlayItem(new GeoPoint(goals.get(i).getGoalLat(),goals.get(i).getGoalLon())
+			,goals.get(i).getGoalTitle(),""));
+		}
+		/*
+		items.add(new OverlayItem(new GeoPoint(43664300,-79399400)
+			,"Roberts Library","ALable2"));
+		items.add(new OverlayItem(new GeoPoint(43662700,-79401200)
+			,"Athlete Centre","BLable2"));
+		items.add(new OverlayItem(new GeoPoint(43663500,-79401500)
+			,"Grad House","CLable2"));
+		*/
 		
 		populate();
 		
@@ -69,5 +83,13 @@ public class CurrentGameOverlay extends ItemizedOverlay<OverlayItem>{
 		}
       return( true);
     }
+    
+    public List<OverlayItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<OverlayItem> items) {
+		this.items = items;
+	}
 	
 }
