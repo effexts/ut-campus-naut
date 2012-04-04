527166b716ad4afde7d5eb38fe2841cb90c6dca7
stevech.hu@gmail.com <stevech.hu@gmail.com@81df9bfd-6d4b-5af3-99e5-d8d13bcfe5b9>
2012-04-04 05:31:59 +0000

    Clean up code for GPS

--- trunk/GPSController/gen/ut/ece1778/GPSController/R.java
+++ trunk/GPSController/gen/ut/ece1778/GPSController/R.java
@@ -15,16 +15,26 @@ public final class R {
     }
     public static final class id {
         public static final int down=0x7f050002;
-        public static final int left=0x7f050004;
+        public static final int left=0x7f050005;
+        public static final int leo=0x7f050008;
         public static final int relativeLayout1=0x7f050000;
-        public static final int right=0x7f050003;
+        public static final int reset=0x7f050003;
+        public static final int right=0x7f050004;
+        public static final int steve=0x7f050007;
         public static final int top=0x7f050001;
+        public static final int user=0x7f050006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int down=0x7f040005;
+        public static final int left=0x7f040003;
+        public static final int leo=0x7f040002;
+        public static final int reset=0x7f040007;
+        public static final int right=0x7f040004;
+        public static final int steve=0x7f040001;
+        public static final int up=0x7f040006;
     }
 }
